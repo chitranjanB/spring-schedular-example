855c127c12e64e3788309c8a59cdfd9e9ad3d573
Julius Krah <juliuskrah@gmail.com>
2016-02-24 19:39:26 +0000

    added scheduling

--- src/main/java/com/jipasoft/web/IndexController.java
+++ src/main/java/com/jipasoft/web/IndexController.java
@@ -2,6 +2,7 @@ package com.jipasoft.web;
 
 import java.util.UUID;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		return "Next random is " + UUID.randomUUID();
+	}
+
+	@Scheduled(fixedRate = 1000)
+	public void message() {
+		System.out.println("prints every 1000ms");
 	}
 }
