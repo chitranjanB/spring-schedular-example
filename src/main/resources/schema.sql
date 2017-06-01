5990b45188826860e663df325becc4d582825fd3
Julius Krah <juliuskrah@gmail.com>
2017-06-01 20:23:41 +0000

    Updated schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,6 +7,6 @@ CREATE TABLE users(
 CREATE TABLE authorities (
 	username varchar_ignorecase(50) NOT NULL,
 	authority varchar_ignorecase(50) NOT NULL,
-	CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username)
+	CONSTRAINT fk_authorities_users FOREIGN KEY(username) REFERENCES users(username) ON UPDATE CASCADE ON DELETE CASCADE
 );
 CREATE UNIQUE INDEX ix_auth_username ON authorities (username,authority);
